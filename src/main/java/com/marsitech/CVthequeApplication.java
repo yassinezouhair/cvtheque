5420f3871e9f0481536db855a3876d809c9030b3
yassinezouhair <yassine.zouhair11@gmail.com>
2020-06-11 22:44:58 +0100

    first commit

---
+++ src/main/java/com/marsitech/CVthequeApplication.java
@@ -0,0 +1,13 @@
+package com.marsitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVthequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CVthequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marsitech/CVthequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CVthequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
